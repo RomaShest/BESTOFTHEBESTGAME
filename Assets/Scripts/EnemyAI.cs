19bcfcac017c65e15f872dfb603a61fb22f3ab23
RomaShest <romabatyapro@gmail.com>
2024-03-03 11:13:08 +0300

    4

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,13 +6,10 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> patrolPoints;
-
     public PlayerController player;
-
     public float viewAngle;
-
+    public float damage = 30;
     private bool _isPlayerNoticed;
-
     private NavMeshAgent _navMeshAgent;
 
     void Start()
@@ -21,11 +18,24 @@ public class EnemyAI : MonoBehaviour
 
         _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
     }
-
-    void Update()
+    private void Update()
+    {
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        PatrolUpdate();
+        AttackUpdate();
+    }
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+    private void NoticePlayerUpdate()
     {
         var direction = player.transform.position - transform.position;
-
+        _isPlayerNoticed = false;
         if (Vector3.Angle(transform.forward, direction) < viewAngle)
         {
             RaycastHit hit;
@@ -35,32 +45,29 @@ public class EnemyAI : MonoBehaviour
                 {
                     _isPlayerNoticed = true;
                 }
-                else
-                {
-                    _isPlayerNoticed = false;
-                }
-            }
-            else
-            {
-                _isPlayerNoticed = false;
             }
         }
-        else
-        {
-            _isPlayerNoticed = false;
-        }
-        if (_isPlayerNoticed)
-        {
-            _navMeshAgent.destination = player.transform.position;
-        }
+    }
 
+    private void PatrolUpdate()
+    {
         if (!_isPlayerNoticed)
         {
-           if (_navMeshAgent.remainingDistance == 0)
-           {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
                 _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
-           }
+            }
         }
-        
+    }
+
+    private void AttackUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                player.GetComponent<PlayerHealth>().DealDamage(damage * Time.deltaTime);
+            }
+        }
     }
 }
